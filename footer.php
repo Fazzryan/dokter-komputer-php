2e1b4bb850c7e67675636a8e9cc7f44bfe67f5aa
Fazryan <dinda5657@gmail.com>
2023-05-31 13:40:59 +0700

    membuat tampilan footer

--- footer.php
+++ footer.php
@@ -4,25 +4,21 @@
             <div class="col-md-6">
                 <h1>DOKTER <span style="color:var(--txt-color-blue);">KOMPUTER</span></h1>
                 <p>&copy; Copyright 2022. Dibuat oleh <a href="https://github.com/fazzryan" class="text-cansel">Dinda Fazryan</a></p>
-                <!-- <p>&copy; Copyright 2022. Dokter Komputer Team</p> -->
             </div>
             <div class="col-md-3">
                 <p class="fw-500">Dokter Komputer</p>
                 <ul class="list-unstyled">
                     <li class="my-2">
-                        <a href="#" class="footer-link">Home</a>
+                        <a href="index.php" class="footer-link">Home</a>
                     </li>
                     <li class="my-2">
-                        <a href="#" class="footer-link">Produk</a>
+                        <a href="produk.php" class="footer-link">Produk</a>
                     </li>
                     <li class="my-2">
-                        <a href="#" class="footer-link">Service</a>
+                        <a href="service.php" class="footer-link">Service</a>
                     </li>
                     <li class="my-2">
-                        <a href="#" class="footer-link">Konsul</a>
-                    </li>
-                    <li class="my-2">
-                        <a href="#" class="footer-link">Tentang Kami</a>
+                        <a href="about.php" class="footer-link">Tentang Kami</a>
                     </li>
                 </ul>
             </div>
