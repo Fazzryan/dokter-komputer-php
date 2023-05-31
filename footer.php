fde5a7013e7e7e652bf70925c1c9d1da7a392084
Fazryan <dinda5657@gmail.com>
2023-05-31 13:39:25 +0700

    membuat file footer.php

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer style="background-color: #0C1F33;">
+    <div class="container py-5 text-white">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>DOKTER <span style="color:var(--txt-color-blue);">KOMPUTER</span></h1>
+                <p>&copy; Copyright 2022. Dibuat oleh <a href="https://github.com/fazzryan" class="text-cansel">Dinda Fazryan</a></p>
+                <!-- <p>&copy; Copyright 2022. Dokter Komputer Team</p> -->
+            </div>
+            <div class="col-md-3">
+                <p class="fw-500">Dokter Komputer</p>
+                <ul class="list-unstyled">
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Home</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Produk</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Service</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Konsul</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <p class="fw-500">Sosial Media</p>
+                <ul class="list-unstyled">
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Instagram</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Facebook</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="#" class="footer-link">Youtube</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
