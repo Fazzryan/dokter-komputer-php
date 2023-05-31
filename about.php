de4271ba36e152256ee9998b1f92c53771bbfedb
Fazryan <dinda5657@gmail.com>
2023-05-31 13:43:29 +0700

    meng include file footer ke setiap  halaman

--- about.php
+++ about.php
@@ -120,50 +120,7 @@ $jumlah_produk = show("SELECT COUNT(*) AS jml_produk FROM produk");
         </div>
     </section>
     <!-- Footer -->
-    <footer style="background-color: #0C1F33;">
-        <div class="container py-5 text-white">
-            <div class="row">
-                <div class="col-md-6">
-                    <h1>DOKTER <span style="color:var(--txt-color-blue);">KOMPUTER</span></h1>
-                    <p>&copy; Copyright 2022. Dibuat oleh <a href="https://github.com/fazzryan" class="text-cansel">Dinda Fazryan</a></p>
-                </div>
-                <div class="col-md-3">
-                    <p class="fw-500">Dokter Komputer</p>
-                    <ul class="list-unstyled">
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Home</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Produk</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Service</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Konsul</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Tentang Kami</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-3">
-                    <p class="fw-500">Sosial Media</p>
-                    <ul class="list-unstyled">
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Instagram</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Facebook</a>
-                        </li>
-                        <li class="my-2">
-                            <a href="#" class="footer-link">Youtube</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include "footer.php" ?>
 
     <script src="asset/js/bootstrap.js"></script>
     <script src="asset/js/script.js"></script>
